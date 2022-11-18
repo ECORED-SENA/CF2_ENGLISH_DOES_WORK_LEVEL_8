a601911d8300101ee68eb56e2f3c02a2103cf73c
elrilacho <camilobr17@gmail.com>
2022-11-18 00:15:26 -0500

    Ajustes AD cargadas

--- src/views/Tema1.vue
+++ src/views/Tema1.vue
@@ -993,6 +993,25 @@
     p.mb-5(data-aos='fade-right') 
       span.r--1.fst-italic Now, read the following text and answer the questions below. 
       span(style='color: #777777') / Ahora, lea el siguiente texto y responda las preguntas relacionadas.
+    .titulo-segundo.color-acento-contenido(data-aos='fade-right')
+      h2 Actividad Didáctica 
+    .row.mb-5.justify-content-center.align-items-center.align-items-lg-stretch
+      .col-6.col-md-4.col-lg-3.mb-4.mb-md-0
+        .tarjeta.h-100.d-flex.align-items-center.p-4
+          figure
+            img(src="@/assets/template/arrastrar.svg", alt="Texto que describa la imagen")
+      .col-12.col-md-8.col-lg-9
+        .titulo-segundo
+          h2 Conectors
+        p.mb-4 Escuche los audios y complete los espacios en blanco de cada una de las frases arrastrando el término correcto.
+        .tarjeta.actividad.p-3
+          .row.justify-content-around.align-items-center            
+            .col-sm.mb-3.mb-sm-0
+              p.fw-bold.mb-0 Arrastrar y soltar
+            .col-auto
+              a.boton.boton--b(:href="obtenerLink('/actividades/a1/story.html')" target="_blank")
+                span Realizar
+                i.fas.fa-puzzle-piece
 
             
           
